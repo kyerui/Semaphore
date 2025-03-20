d9771ba59a16ff332ca50938132cf420dcb38084
Gael Huk Kukla <gael.kukla@outlook.com>
2025-03-20 13:44:55 -0300

    Update SemaforoExemplo.java

--- SemaforoExemplo.java
+++ SemaforoExemplo.java
@@ -3,10 +3,8 @@ import java.util.concurrent.Semaphore;
 public class SemaforoExemplo {
 
     public static void main(String[] args) {
-        // Cria um semáforo com 3 permissões (permite até 3 threads acessarem o recurso simultaneamente)
         Semaphore semaforo = new Semaphore(3);
 
-        // Cria e inicia várias threads que tentarão acessar o recurso
         for (int i = 1; i <= 10; i++) {
             Thread thread = new Thread(new Tarefa(semaforo, i));
             thread.start();
@@ -27,17 +25,14 @@ public class SemaforoExemplo {
             try {
                 System.out.println("Thread " + id + " está tentando adquirir uma permissão.");
 
-                // Adquire uma permissão do semáforo
                 semaforo.acquire();
 
                 System.out.println("Thread " + id + " adquiriu uma permissão.");
 
-                // Simula o uso do recurso compartilhado
                 Thread.sleep(2000);
 
                 System.out.println("Thread " + id + " está liberando a permissão.");
 
-                // Libera a permissão do semáforo
                 semaforo.release();
 
             } catch (InterruptedException e) {
